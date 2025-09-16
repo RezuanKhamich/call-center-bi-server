ccf7cd6bdf003826adbd22cf0fdd6e35c5e61d5f
RezuanKhamich <Khamukov19@gmail.com>
2025-09-16 11:26:46 +0300

    release/1.0.1 - added mo role routes

--- index.js
+++ index.js
@@ -17,13 +17,15 @@ app.use(cors({ origin: 'http://localhost:5173', credentials: false }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+app.get('/api', (req, res) => {
+  res.json({ status: 'ok', message: 'API is working' });
+});
+
 app.use('/api', authRoutes);
 app.use('/api/moderator', moderatorRoutes);
 app.use('/api/mo', moRoutes);
 app.use('/api/minister', ministerRoutes);
 
-const allUsers = await prisma.users.findMany();
-console.log('user', allUsers)
 const PORT = process.env.PORT || 5009;
 
 app.listen(PORT, () => {
--- routes/moRoutes.mjs
+++ routes/moRoutes.mjs
@@ -1,12 +1,107 @@
 import express from 'express';
 import { authenticateJWT, authorizeRoles } from '../middleware/authMiddleware.mjs';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const router = express.Router();
-
 router.use(authenticateJWT, authorizeRoles('mo'));
 
 router.get('/dashboard', (req, res) => {
   res.json({ message: 'MO dashboard' });
+});
+
+// GET /api/mo — получить список медорганизаций
+router.get('/get-mo-list', async (req, res) => {
+  try {
+    const organizations = await prisma.med_organizations.findMany({
+      orderBy: { name: 'asc' }, // сортировка по названию
+    })
+
+    res.json(organizations)
+  } catch (error) {
+    console.error('❌ Ошибка получения МО:', error)
+    res.status(500).json({ message: 'Ошибка сервера' })
+  }
+})
+
+router.get('/reports-by-date', async (req, res) => {
+  try {
+    const {
+      reporting_period_start_date,
+      reporting_period_end_date,
+      mo_id,
+    } = req.query;
+
+    const where = {};
+
+    if (reporting_period_start_date) {
+      where.reporting_period_start_date = {
+        gte: new Date(reporting_period_start_date),
+      };
+    }
+
+    if (reporting_period_end_date) {
+      where.reporting_period_end_date = {
+        lte: new Date(reporting_period_end_date),
+      };
+    }
+
+    // Для выбранной МО — все поля
+    const selectedMoReports = await prisma.reports.findMany({
+      where: {
+        ...where,
+        mo_id: Number(mo_id),
+      },
+      orderBy: [
+        { reporting_period_end_date: 'desc' },
+        { reporting_period_start_date: 'desc' },
+      ],
+    });
+
+    // Для остальных МО — только department и status
+    const otherMoReports = await prisma.reports.findMany({
+      where: {
+        ...where,
+        NOT: { mo_id: Number(mo_id) },
+      },
+      select: {
+        id: true, // чтобы было уникальное поле в массиве
+        department: true,
+        created_at: true,
+        agency_type: true,
+        status: true,
+      },
+      orderBy: [
+        { reporting_period_end_date: 'desc' },
+        { reporting_period_start_date: 'desc' },
+      ],
+    });
+    const reports = [ ...selectedMoReports, ...otherMoReports ]
+
+    res.json(reports);
+  } catch (error) {
+    console.error('❌ Ошибка получения отчетов по диапазону и МО:', error);
+    res.status(500).json({ message: 'Ошибка сервера' });
+  }
+});
+
+router.get('/get-users', async (req, res) => {
+  try {
+    const users = await prisma.users.findMany({
+      select: {
+        id: true,
+        full_name: true,
+      },
+      orderBy: {
+        full_name: 'asc',
+      },
+    });
+
+    res.json(users);
+  } catch (error) {
+    console.error('❌ Ошибка получения пользователей:', error);
+    res.status(500).json({ message: 'Ошибка сервера' });
+  }
 });
 
 export default router;
